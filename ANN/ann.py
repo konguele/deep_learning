4c868416f11d559263ce08f265a8582e5afa8089
konguele <juanjo.gmanzano@gmail.com>
2022-06-10 13:59:29 +0200

    test

---
+++ ANN/ann.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 10 11:14:56 2022
+
+@author: JuanJo García
+"""
+
+# Redes Neuronales Artificales
+
+# Instalar Theano (desde Anaconda prompt)
+# conda install theano
+
+# Instalar Tensorflow y Keras
+# conda install -c conda-forge keras
+
+# Parte 1 - Pre procesado de datos
+
+# Cómo importar las librerías
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importar el data set
+dataset = pd.read_csv('Churn_Modelling.csv')
+
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Codificar datos categóricos
+from sklearn.preprocessing import OneHotEncoder
+onehotencoder = OneHotEncoder(categorical_features=[1])
+X = onehotencoder.fit_transform(X).toarray()
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+transformer = ColumnTransformer(
+    transformers=[
+        ("Churn_Modelling",        # Un nombre de la transformación
+         OneHotEncoder(categories='auto'), # La clase a la que transformar
+         [1]            # Las columnas a transformar.
+         )
+    ], remainder='passthrough'
+)
+
+X = transformer.fit_transform(X)
+X = X[:, 1:]
+
+# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Escalado de variables
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+# Parte 2 - Construir la RNA
+
+#Importar Keras y librerías adicionales
+
+import Keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Inicializar la RNA
+classifier = Sequential()
